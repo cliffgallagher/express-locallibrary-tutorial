56aca42ba6426e5d2223dfcee95e5c72a038ffe5
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-04 09:49:28 -0500

    added Book.js model

---
+++ models/Book.js
@@ -0,0 +1,57 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const Author = require('./Author');
+const Genre = require('./Genre');
+const sequelize = new Sequelize('local_library', 'root', 'JosieOvechkin89!', {
+    dialect: 'mysql'
+});
+
+module.exports = sequelize.define('Book', {
+    book_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: Author,
+            key: 'author_id'
+        }
+    },
+    summary: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isbn: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: Genre,
+            key: 'genre_id'
+        }
+    }
+}, {
+    tableName: books
+});
+
+/*const test = async () => {
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  sequelize.close();
+};
+
+test();*/

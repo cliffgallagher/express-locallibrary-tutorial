551d246fafde20dc73e711fa0a7b4f7e5c712a87
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-04 15:17:27 -0500

    seed_genres.js works

---
+++ seeders/20211004191741-seed_genres.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('genres', [{
+        name: "Fantasy",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: "Science Fiction",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: "French Poetry",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('genres', null, {});
+  }
+};

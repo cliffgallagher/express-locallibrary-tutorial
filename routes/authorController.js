845bb20ca2cbffe415f6192d3d1987d40d8797af
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-05 10:21:46 -0500

    fixed typo

--- routes/authorController.js
+++ routes/authorController.js
@@ -16,7 +16,7 @@ exports.author_create_get = function(req, res) {
 }
 
 // Handle Author create on POST
-exports.author_create_post(req, res) {
+exports.author_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author create POST');
 }
 
@@ -33,9 +33,9 @@ exports.author_delete_post = function(req, res) {
 // Display Author update form on GET
 exports.author_update_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update GET');
-};
+}
 
 // Handle Author update on POST.
 exports.author_update_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update POST');
-};
+}

6d3ad3c8190d3d9113d9be80eaf381f8344a464b
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-08 11:38:26 -0500

    added basic css styling so you can tell elements from database apart

--- client/src/components/DataItem.js
+++ client/src/components/DataItem.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
+import './DataItem.css';
 
 const DataItem = (props) => {
     return (
-        <div>
+        <div className='dataItem'>
             <p>{props.book_id}</p><p>{props.title}</p><p>{props.author_id}</p>      
         </div>
     );

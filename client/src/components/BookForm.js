81f5c20abe3476f9ea7a5c5b65aa6b8a9cb245ba
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-12 18:40:11 -0500

    authors input of BookForm pulls static array but I think BookForm is rendering twice

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,4 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
 const BookForm = (props) => {
+
+    const [authorOptions, setAuthorOptions] = useState();
+
+    const options = [
+        {
+            text: "Herman Melville",
+            value: "1"
+        }, {
+            text: "James Joyce",
+            value: "2"
+        }, {
+            text: "Mitch Albom",
+            value: "3"
+        }
+    ];
+
+    if (options != null) {
+        console.log("options exists");
+    }
+
+    /*useEffect(() => {
+        options.forEach(option =>
+            optionsList.add(
+                new Option(option.text, option.value)
+            )   
+    )}, []);*/
+
+    
+    useEffect(() => {
+        setAuthorOptions(() => {
+            return options.map(option => <option value={option.value}>{option.text}</option>);
+        })
+    }, [])
+    
 
     function bookFormSubmitHandler(event) {
         event.preventDefault();
@@ -8,6 +44,7 @@ const BookForm = (props) => {
             <label>Title<input type='text' name='titleField'/></label>
             <label>ISBN<input type='text' name='isbnField' /></label>
             <label>Summary<input type='text' name='summaryField' /></label>
+            <label>Authors<select name='authors'>{authorOptions}</select></label>
             <button onClick={props.onCancel}>Cancel</button>
             <button type='submit'>Submit</button>
         </form>

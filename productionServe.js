03dc3eba1607417162aba3eb6c872d8e39f495d9
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-14 13:38:01 -0500

    added router to productionServe.js, don't know if that's right to do

--- productionServe.js
+++ productionServe.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', function (req, res) {
+// GET catalog home page.
+app.use('/catalog', catalogRouter);
+
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 

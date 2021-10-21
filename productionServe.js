5957a7f3ee9bd3d04a5285b283d71aa73efc7cc1
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-10-21 14:21:55 -0500

    added app.use(express.json()) and POST request works now

--- productionServe.js
+++ productionServe.js
@@ -1,12 +1,14 @@
 const express = require('express');
+var cookieParser = require('cookie-parser');
 const path = require('path');
-const app = express();
 const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const app = express();
 
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-// GET catalog calls
+// catalog calls
 app.use('/catalog', catalogRouter);
 app.use('/catalog/*', catalogRouter);
 

47d1695ae679c80c6d552f67b449d172adc2059a
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-03 12:36:36 -0500

    update and delete buttons appear when hover over AuthorListElement

--- client/src/components/author_components/AuthorListElement.js
+++ client/src/components/author_components/AuthorListElement.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthorInfo from './AuthorInfo';
 import '../ListElement.css'
 
 const AuthorListElement = (props) => {
+    const [displayUpdateAndDeleteButtons, setDisplayUpdateAndDeleteButtons] = useState(false);
+
+    function showUpdateAndDeleteButtons() {
+        setDisplayUpdateAndDeleteButtons(true);
+    }
+
+    function hideUpdateAndDeleteButtons() {
+        setDisplayUpdateAndDeleteButtons(false);
+    }
+
+    function updateBookButtonClickHandler() {
+        props.setDisplayBookPopupForUpdate(true);
+        props.bookPropsFromBookListElementToBookComponent(props.bookID, props.authorID, props.genreID);
+    }
+
+    function deleteBookButtonClickHandler() {
+        props.setDisplayBookPopupForDelete(true);
+        props.bookPropsFromBookListElementToBookComponent(props.bookID, props.authorID, props.genreID);
+    }
+    
     return (
-        <div className='listElement'>
-            <AuthorInfo firstName={props.firstName} familyName={props.familyName} dateOfBirth={props.dateOfBirth} dateOfDeath={props.dateOfDeath}/>
+        <div className='listElement' onMouseEnter={showUpdateAndDeleteButtons} onMouseLeave={hideUpdateAndDeleteButtons}>
+            {!displayUpdateAndDeleteButtons && <AuthorInfo firstName={props.firstName} familyName={props.familyName} dateOfBirth={props.dateOfBirth} dateOfDeath={props.dateOfDeath}/>}
+            {displayUpdateAndDeleteButtons && (
+                <div>
+                    <AuthorInfo firstName={props.firstName} familyName={props.familyName} dateOfBirth={props.dateOfBirth} dateOfDeath={props.dateOfDeath}/>
+                    <button>Update Author</button><button>Delete Author</button>
+                </div>
+            )}
         </div>
     )
 }

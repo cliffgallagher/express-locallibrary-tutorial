d294bcfbe0b2e017d76b1e223c61b711a1bc7dc6
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-04 13:17:09 -0500

    added some error handling to productionServe

--- productionServe.js
+++ productionServe.js
@@ -16,4 +16,30 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// handle SequelizeForeignKeyConstraint errors
+app.use((error, req, res, next) => {
+  if (error.name === 'SequelizeForeignKeyConstraintError') {
+    console.log("entered sequelize error handler");
+    res.json('SequelizeForeignKeyConstraintError');
+  } else {
+    next()
+  }
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen(9000);

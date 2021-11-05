774dd4af6eb30b39cb374fe3db0e326c1fbe5f66
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-05 14:27:07 -0500

    believe binarySearch should work for object properties that have string values

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,32 @@
+/*
+arr is the array you're searching, l is begininning index (0), r is ending index, x is value you're searching for, k is name of object keys you are searching (as string)
+*/
 
+function binarySearchBookObjects(arr, l, r, x, k){
+    if (r >= l) {
+        let mid = l + Math.floor((r - l) / 2);
+ 
+        // If the element is present at the middle
+        // itself
+        if (arr[mid][k] === x)
+            return mid;
+ 
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid][k] > x)
+            return binarySearchBookObjects(arr, l, mid - 1, x, k);
+ 
+        // Else the element can only be present
+        // in right subarray
+        return binarySearchBookObjects(arr, mid + 1, r, x, k);
+    }
+ 
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+
+module.exports = binarySearchBookObjects;
 
 // JavaScript program to implement recursive Binary Search
  
@@ -28,31 +56,3 @@
     // present in array
     return -1;
 }*/
-
-function binarySearchBookObjects(arr, l, r, x, k){
-    if (r >= l) {
-        let mid = l + Math.floor((r - l) / 2);
- 
-        // If the element is present at the middle
-        // itself
-        if (arr[mid][k] === x)
-            return mid;
- 
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (arr[mid][k] > x)
-            return binarySearchBookObjects(arr, l, mid - 1, x, k);
- 
-        // Else the element can only be present
-        // in right subarray
-        return binarySearchBookObjects(arr, mid + 1, r, x, k);
-    }
- 
-    // We reach here when element is not
-    // present in array
-    return -1;
-}
-
-console.log(binarySearchBookObjects([{value: "titleOne"}, {value: "titleTwo"}, {value: "titleThree"}, {value: "titleFour"}], 0, 3, "titleOne", "value"));
-
-module.exports = binarySearchBookObjects;

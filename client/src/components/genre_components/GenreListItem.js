4c858c7d88690ccace8efa048c25adb639ca9503
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-05 06:50:39 -0500

    update and delete buttons appear when hover over genre

--- client/src/components/genre_components/GenreListItem.js
+++ client/src/components/genre_components/GenreListItem.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import GenreInfo from './GenreInfo';
 import '../ListElement.css';
 
 const GenreListItem = (props) => {
+    const [showUpdateAndDeleteButtons, setShowUpdateAndDeleteButtons] = useState(false);
+
+    function mouseEnterGenreListItemHandler() {
+        setShowUpdateAndDeleteButtons(true);
+    }
+
+    function mouseLeaveGenreListItemHandler() {
+        setShowUpdateAndDeleteButtons(false);
+    }
+    
     return (
-        <div className='listElement'>
-            <GenreInfo genreName={props.genreName}/>
+        <div className='listElement' onMouseEnter={mouseEnterGenreListItemHandler} onMouseLeave={mouseLeaveGenreListItemHandler}>
+            {!showUpdateAndDeleteButtons && <GenreInfo genreName={props.genreName}/>}
+            {showUpdateAndDeleteButtons && (
+                <div>
+                    <GenreInfo genreName={props.genreName}/>
+                    <button>Update Genre</button><button>Delete Genre</button>
+                </div>
+            )}
         </div>
     )
 }

cb11736f3664080a15767413a42ff63e70184ce1
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-11 08:54:55 -0600

    empty summary validator works

--- routes/catalog.js
+++ routes/catalog.js
@@ -29,7 +29,6 @@ router.get('/book/create', book_controller.book_create_get);
 // POST requests for creating Book.
 router.post('/book/create/one', 
 body('title').not().isEmpty().withMessage("Title cannot be blank"),
-//body('isbn').isNumeric({no_symbols: true}).withMessage("ISBN can only contain numerals"),
 body('isbn').custom((value) => {
     if (value.toLowerCase().search(/\D/) !== -1) {
         throw new Error("ISBN can contain only numbers");
@@ -38,11 +37,12 @@ body('isbn').custom((value) => {
 }),
 body('isbn').custom((value) => {
     if ((value.length !== 0) && (value.length !== 10) && (value.length !== 13)) {
-        console.log("entered if block of custom validator");
+        //console.log("entered if block of custom validator");
         throw new Error("ISBN must be 0, 10 or 13 numbers long");
     }
     return true;
 }),
+body('summary').not().isEmpty().withMessage("Summary cannot be blank"),
 
 
 function(req, res, next) {

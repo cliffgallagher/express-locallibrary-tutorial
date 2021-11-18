38cfbf2f52017f81e6241b7b3dabef7deb2edf21
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-18 09:57:20 -0600

    added some test handler functions to see if I could click on different things

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
+
+    function sayHello() {
+        console.log("hi I'm fabars");
+    }
+    
     return (
         <>
             <div className="navbar">
-                <FaBars />
+                <FaBars onClick={sayHello}/>
             </div>
         </>
 
--- client/src/components/NavbarOptions.js
+++ client/src/components/NavbarOptions.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
 const NavbarOptions = () => {
+    
+    function helloBooks() {
+        console.log("hello books");
+    }
+
+    function helloAuthors() {
+        console.log("hello authors");
+    }
+    
     return (
         <div>
             <AiOutlineClose />
             <ul>
-                <li>Books</li>
-                <li>Authors</li>
+                <li onClick={helloBooks}>Books</li>
+                <li onClick={helloAuthors}>Authors</li>
                 <li>Genres</li>
             </ul>
         </div>

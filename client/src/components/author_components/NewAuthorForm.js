94b2cdfc31e449bead63c55a34a347d8a6bb0749
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-11-20 06:52:49 -0600

    duplicate author warning works for new authors

--- client/src/components/author_components/NewAuthorForm.js
+++ client/src/components/author_components/NewAuthorForm.js
@@ -5,6 +5,7 @@ const NewAuthorForm = (props) => {
     const [newAuthorFamilyName, setNewAuthorFamilyName] = useState();
     const [newAuthorBirthDate, setNewAuthorBirthDate] = useState();
     const [newAuthorDeathDate, setNewAuthorDeathDate] = useState();
+    const [validationErrors, setValidationErrors] = useState();
 
     function newAuthorFormFirstNameChangeHandler(event) {
         setNewAuthorFirstName(event.target.value);
@@ -50,13 +51,30 @@ const NewAuthorForm = (props) => {
             props.hideNewAuthorForm();
             const data = await response.json();
             //console.log("data: " + data);
-            if (data === "author already present in database") {
+            /*if (data === "author already present in database") {
                 //console.log("yes, author is in database");
                 props.showDuplicateAuthorWarning();
             } else {
                 //console.log("author not already in list, was inserted");
-                props.getAuthorList();
-            }
+                props.getAuthorList();*/
+                if (typeof data === 'object') {
+                    if (data.hasOwnProperty('errors')) {
+                        //console.log("data.errors: " + JSON.stringify(data.errors));
+                        const errorMessages = data.errors.map(element => element.msg);
+                        if (errorMessages.includes("author already in database")) {
+                            props.showDuplicateAuthorWarning(true);
+                        } else {
+                            //console.log("errorMessages: " + JSON.stringify(errorMessages));
+                            setValidationErrors(() => {
+                                return errorMessages.map(element => <li>{element}</li>);
+                            });
+                        }
+                        //console.log("errorMessages: " + JSON.stringify(errorMessages));
+                    } else {
+                        props.getAuthorList();
+                        return response;
+                    }
+                }
         } catch(e) {
             console.log(e);
         }

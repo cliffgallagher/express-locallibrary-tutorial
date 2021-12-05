69b7bc29b0ac7f7e3a27f77a4dd74397692c1206
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-05 11:52:58 -0600

    NewUserSignup is styled

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
           </div>
         )}
         {!isLoggedIn && isNewUser && (
-          <NewUserSignup />
+          <NewUserSignup setIsNewUser={setIsNewUser}/>
         )}
         {isLoggedIn && (
           <div>
--- client/src/components/user_components/NewUserSignup.js
+++ client/src/components/user_components/NewUserSignup.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { AiFillPropertySafety } from 'react-icons/ai';
 import styles from './NewUserSignup.module.css';
 
-const NewUserSignup = () => {
+const NewUserSignup = (props) => {
+    
+    function backToLoginClickHandler() {
+        props.setIsNewUser(false);
+    }
+    
     return (
         <div className={styles.popup}>
             <div className={styles.popup_inner}>
@@ -9,10 +15,14 @@ const NewUserSignup = () => {
                 <form className={styles.form}>
                     <label>First Name<input type='text' name='newuserUsername' /></label>
                     <label>Last Name<input type='text' name='newUserPassword' /></label>
+                    <label>Email<input type='text' name='newUserPassword' /></label>
+                    <label>Password<input type='text' name='newUserPassword' /></label>
+                    <label>Confirm Password<input type='text' name='newUserPassword' /></label>
                     <div id={styles.button_div}>
-                        <button>Login</button>
+                        <button>Create User</button>
                     </div>
                 </form>
+                <a onClick={backToLoginClickHandler}>Back to Login</a>
             </div>
         </div>
     )

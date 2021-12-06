b7f94478e223fd17e888d1f5f18c8fa580029852
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-06 14:48:28 -0600

    added User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require("../config/database");
+
+module.exports = db.define('User', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});

b563c0909b50561e858a49085505fead7c82ba38
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-08 10:00:30 -0600

    hashed password is saved in users table

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,22 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 exports.user_create_post = async function(req, res) {
-    console.log('req.body in user_create_get')
-    const newUser = await User.create({
-        first_name: req.body.newUserFirstName,
-        last_name: req.body.newUserLastName,
-        email: req.body.newUserEmail,
-        password: req.body.newUserPassword
-    })
-    res.json(newUser);
+    //console.log('req.body in user_create_get')
+
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.newUserPassword, salt)
+        const newUser = await User.create({
+            first_name: req.body.newUserFirstName,
+            last_name: req.body.newUserLastName,
+            email: req.body.newUserEmail,
+            password: hashedPassword
+        })
+        res.json(newUser);
+
+    } catch(e) {
+        console.log(e)
+    }
+
 } 

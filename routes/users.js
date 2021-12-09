01ceb82e09fa8f84a8fad94463ae3a8629830f51
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-09 08:57:23 -0600

    password confirmation fixed

--- routes/users.js
+++ routes/users.js
@@ -14,22 +14,22 @@ router.get('/cool/', function(req, res, next) {
 
 router.post('/create',
 body('newUserFirstName').not().isEmpty().withMessage("First name cannot be blank"),
-/*body('newUserLastName').not().isEmpty().withMessage("Last name cannot be blank"),
+body('newUserLastName').not().isEmpty().withMessage("Last name cannot be blank"),
 body('newUserEmail').isEmail().withMessage('Please enter a valid email address'),
 body('newUserPassword').isLength({
   min: 8,
   max: 16
 }).withMessage('Password must be between 8 and 16 characters long'),
-body('newUserPassword').isStrongPassword().withMessage('Password must contain 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character')),
+body('newUserPassword').isStrongPassword().withMessage('Password must contain 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character'),
 body('newUserConfirmedPassword').custom((value, { req }) => {
-  if (value !== req.body.password) {
+  if (value !== req.body.newUserPassword) {
     throw new Error('Password confirmation does not match password');
   }
   return true;
-}),*/
+}),
 
 function(req, res, next) {
-  console.log("inside user/create validation function: " + JSON.stringify(req.body))
+  //console.log("inside user/create validation function: " + JSON.stringify(req.body))
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });

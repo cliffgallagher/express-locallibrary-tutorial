adde52d197368a70aec22b8a9cb870511e8b8894
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-15 09:08:35 -0600

    author search works for first or last names but not both

--- client/src/components/author_components/AuthorListElement.js
+++ client/src/components/author_components/AuthorListElement.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import AuthorInfo from './AuthorInfo';
 import styles from '../ListElement.module.css'
 import ChosenAuthor from './ChosenAuthor.js';
+import { AuthContext } from "../../context/auth-context";
 
 const AuthorListElement = (props) => {
     const [displayUpdateAndDeleteButtons, setDisplayUpdateAndDeleteButtons] = useState(false);
     const [hideChosenElement, setHideChosenElement] = useState(false);
     const [displayChosenElement, setDisplayChosenElement] = useState(false);
-
+    const [gridOrder, setGridOrder] = useState(false);
+    const auth = useContext(AuthContext);
     //console.log('date of birth in AuthorListElement: ' + props.dateOfBirth);
     function showUpdateAndDeleteButtons() {
         setDisplayUpdateAndDeleteButtons(true);
@@ -36,9 +38,35 @@ const AuthorListElement = (props) => {
             setHideChosenElement(true);
         }
     }
+
+    function searchForSearchText() {
+        if (auth.searchText) {
+            if (auth.searchText.length > 0 ) {
+                const searchTextToLowerCase = auth.searchText.toLowerCase();
+                if (!(props.firstName.toLowerCase().includes(searchTextToLowerCase)) && !(props.familyName.toLowerCase().includes(searchTextToLowerCase))) {
+                    //console.log('no such text found');
+                    setHideChosenElement(true);
+                    setGridOrder(true);
+                } else {
+                    setHideChosenElement(false);
+                    setGridOrder(false);
+                }
+            } else {
+                setHideChosenElement(false);
+                setGridOrder(false);
+            }
+        } else {
+            setHideChosenElement(false);
+            setGridOrder(false);
+        }
+    }
+
+    useEffect(() => {
+        searchForSearchText();
+    }, [auth.searchText]);
     
     return (
-        <div className={`${styles.listElement} ${hideChosenElement ? styles.hideElement: ''}`} onClick={clickElementHandler}/*{styles.listElement} onMouseEnter={showUpdateAndDeleteButtons} onMouseLeave={hideUpdateAndDeleteButtons}*/>
+        <div className={`${styles.listElement} ${hideChosenElement ? styles.hideElement: ''} ${gridOrder ? styles.gridOrder : ''}`} onClick={clickElementHandler}/*{styles.listElement} onMouseEnter={showUpdateAndDeleteButtons} onMouseLeave={hideUpdateAndDeleteButtons}*/>
             {!displayChosenElement && (
                 <div>
                     <AuthorInfo firstName={props.firstName} familyName={props.familyName} dateOfBirth={props.dateOfBirth} dateOfDeath={props.dateOfDeath}/>

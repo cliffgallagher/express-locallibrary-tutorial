2465bc3bf11f9562ed44bba3be52589d76251831
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-15 10:15:12 -0600

    search function works for genre

--- client/src/components/genre_components/GenreListItem.js
+++ client/src/components/genre_components/GenreListItem.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import GenreInfo from './GenreInfo';
 import styles from '../ListElement.module.css';
 import ChosenGenre from './ChosenGenre.js';
+import { AuthContext } from "../../context/auth-context";
 
 const GenreListItem = (props) => {
     const [showUpdateAndDeleteButtons, setShowUpdateAndDeleteButtons] = useState(false);
     const [displayChosenElement, setDisplayChosenElement] = useState(false);
     const [hideChosenElement, setHideChosenElement] = useState(false);
+    const [gridOrder, setGridOrder] = useState();
+    const auth = useContext(AuthContext);
 
     function mouseEnterGenreListItemHandler() {
         setShowUpdateAndDeleteButtons(true);
@@ -30,9 +33,35 @@ const GenreListItem = (props) => {
         setDisplayChosenElement(true);
         setHideChosenElement(true);
     }
+
+    function searchForSearchText() {
+        if (auth.searchText) {
+            if (auth.searchText.length > 0 ) {
+                const searchTextToLowerCase = auth.searchText.toLowerCase();
+                if (!(props.genreName.toLowerCase().includes(searchTextToLowerCase))) {
+                    //console.log('no such text found');
+                    setHideChosenElement(true);
+                    setGridOrder(true);
+                } else {
+                    setHideChosenElement(false);
+                    setGridOrder(false);
+                }
+            } else {
+                setHideChosenElement(false);
+                setGridOrder(false);
+            }
+        } else {
+            setHideChosenElement(false);
+            setGridOrder(false);
+        }
+    }
+
+    useEffect(() => {
+        searchForSearchText();
+    }, [auth.searchText]);
     
     return (
-        <div className={`${styles.listElement} ${hideChosenElement ? styles.hideElement: ''}`} onClick={clickElementHandler}>
+        <div className={`${styles.listElement} ${hideChosenElement ? styles.hideElement: ''} ${gridOrder ? styles.gridOrder : ''}`} onClick={clickElementHandler}>
             {!displayChosenElement && (
                 <GenreInfo genreName={props.genreName}/>
             )}

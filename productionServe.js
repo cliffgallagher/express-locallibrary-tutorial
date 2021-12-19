6947ccd91226783ea2e60a6ddc1f6ca9f3548fb7
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-19 14:00:11 -0600

    productionServe seems to work for now

--- productionServe.js
+++ productionServe.js
@@ -1,16 +1,21 @@
+require('dotenv').config()
 const express = require('express');
 var cookieParser = require('cookie-parser');
 const path = require('path');
 const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const usersRouter = require('./routes/users');
+const { cookie } = require('express-validator');
 const app = express();
 
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // catalog calls
 app.use('/catalog', catalogRouter);
 app.use('/catalog/*', catalogRouter);
+app.use('/users', usersRouter);
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
@@ -25,21 +30,73 @@ app.use(function(req, res, next) {
 app.use((error, req, res, next) => {
   if (error.name === 'SequelizeForeignKeyConstraintError') {
     //console.log("entered sequelize error handler");
-    res.json('SequelizeForeignKeyConstraintError');
+    res.json({
+      "errors": [
+        {
+          'msg': 'SequelizeForeignKeyConstraintError'
+        }
+      ]
+    });
+    //res.json('SequelizeForeignKeyConstraintError');
   } else {
-    next()
+    //console.log("entered else block of foreign key constraint handler")
+    next(error)
+  }
+})
+
+app.use((error, req, res, next) => {
+  //console.log("entered constraint error handler");
+  if (error.name === 'SequelizeUniqueConstraintError') {
+    //console.log('correctly entered unique constraint block')
+    res.json({
+      "errors": [
+        {
+          'msg': 'SequelizeUniqueConstraintError',
+          'type': error.errors[0].path
+        }
+      ]
+    });
+  } else {
+    //console.log('i am in next block wrongly')
+    next(error)
+  }
+})
+
+app.use((error, req, res, next) => {
+  //console.log("entered constraint error handler");
+  if (error.name === 'TokenExpiredError') {
+    //console.log('correctly entered unique constraint block')
+    res.json(error);
+  } else {
+    //console.log('i am in next block wrongly')
+    next(error)
   }
 })
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  //console.log("entered dfault error handler")
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log('error in default handler: ' + JSON.stringify(err))
+  if (err.name.toLowerCase().includes('error')) {
+    //console.log('entered if block')
+    res.json({
+      "errors": [
+        {
+          'msg': 'Invalid input'
+        }
+      ]
+    });
+  } else {
+    res.end()
+  }
+  //res.render('error');
+
 });
 
 app.listen(9000);

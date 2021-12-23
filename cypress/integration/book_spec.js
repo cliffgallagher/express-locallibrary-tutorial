d53efd8a6d08b44b1857a27e78dc711cfcb30dbb
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-12-23 09:52:30 -0600

    cypress can access my site if I start site in another terminal

---
+++ cypress/integration/book_spec.js
@@ -0,0 +1,9 @@
+describe('user_spec', () => {
+    it('user can add book', () => {
+        //login
+        cy.visit('http://localhost:3000')
+        //click add new book button
+        //enter book information
+        //click "add"
+    })
+})

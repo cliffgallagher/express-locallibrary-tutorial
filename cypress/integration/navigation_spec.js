25c5e12cb2f951b03a30550126faef15f5df4b3d
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-01-04 15:46:35 -0600

    added a couple tests

--- cypress/integration/navigation_spec.js
+++ cypress/integration/navigation_spec.js
@@ -7,12 +7,32 @@ describe('navigation_spec', () => {
             escapedUsername: 'cliffgallagher'.replaceAll('\'', '\\\''),
         })
         cy.wait(1000)
-        cy.visit('/')
+        //cy.visit('/')
     })
 
-    it('beforeEach works', () => {
+    /*it('beforeEach works', () => {
         cy.visit('/')
         cy.contains('Add New Book')
+    })*/
+
+    it('Clicking "Logout" logs you out, and you are still logged out if you try to refresh page', () => {
+        cy.visit('/')
+        cy.get('#root > div > div > div:nth-child(1) > svg').click()
+        cy.findByText(/logout/i).click()
+
+        cy.get('p').should('contain', 'Please log in below')
+
+        cy.visit('/')
+        cy.get('p').should('contain', 'Please log in below')
+    })
+
+    it('After logging in, you stay logged in if you refresh page', () => {
+        cy.visit('/')
+        cy.visit('/')
+        cy.contains('Add New Book')
+        cy.get('#root > div > div > div:nth-child(1) > svg').click()
+        cy.findByText(/logout/i).click()
+        cy.get('p').should('contain', 'Please log in below')
     })
 
 })

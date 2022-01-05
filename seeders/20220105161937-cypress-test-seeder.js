83854af5b5348653c20494718ebf6af941a35f51
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-01-05 11:06:47 -0600

    can seed a single book into database

---
+++ seeders/20220105161937-cypress-test-seeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('books', [{
+      title: 'Pachenko',
+      author_id: 79,
+      summary: 'An epic historical fiction novel following a Korean family that immigrates to Japan.',
+      isbn: '9781455563920',
+      genre_id: 47,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

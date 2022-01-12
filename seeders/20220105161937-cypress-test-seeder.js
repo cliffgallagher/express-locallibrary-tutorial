749e3c0988e4712ec9d67728297f937ee857ff77
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-01-12 08:56:14 -0600

    updated book seeder with new values for author_id and genre_id

--- seeders/20220105161937-cypress-test-seeder.js
+++ seeders/20220105161937-cypress-test-seeder.js
@@ -14,42 +14,42 @@ module.exports = {
 
     await queryInterface.bulkInsert('books', [{
       title: 'A Visit From the Goon Squad',
-      author_id: 34,
+      author_id: 880,
       summary: 'Winner of a Pulitzer in 2011, the book is a set of thirteen interrelated stories with a large set of characters all connected to Bennie Salazar, a record company executive, and his assistant, Sasha.',
       isbn: '0307477479',
-      genre_id: 47,
+      genre_id: 352,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       title: 'Station Eleven',
-      author_id: 75,
+      author_id: 885,
       summary: 'It takes place in the Great Lakes region before and after a fictional swine flu pandemic, known as the "Georgia Flu", has devastated the world, killing most of the population.',
       isbn: '9780804172448',
-      genre_id: 47,
+      genre_id: 352,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       title: 'Metamorphosis',
-      author_id: 96,
+      author_id: 882,
       summary: 'A man turns into a fly.',
       isbn: '9781557427663',
-      genre_id: 47,
+      genre_id: 352,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       title: 'On Beauty',
-      author_id: 71,
+      author_id: 886,
       summary: 'The story follows the lives of a mixed-race British/American family living in the U.S.',
       isbn: '9780143037743',
-      genre_id: 47,
+      genre_id: 352,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       title: 'The Corrections: A Novel',
-      author_id: 81,
+      author_id: 881,
       summary: 'The troubles of an elderly Midwestern couple and their three adult children, tracing their lives from the mid-20th century to "one last Christmas" together near the turn of the millennium.',
       isbn: '0312421273',
-      genre_id: 47,
+      genre_id: 352,
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});

c11356ebc1a95dc47c5de19da31d9ac064ea9a84
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-01-16 17:43:55 -0600

    made database host an environment variable

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
 module.exports = new Sequelize(process.env.MYSQL_SCHEMA_NAME, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql'
 });

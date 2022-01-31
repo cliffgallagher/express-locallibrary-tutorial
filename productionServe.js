3e728f2d10c24414e3db4a914e5c724d8a48dafd
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-01-31 13:01:12 -0600

    deleted CSP configuration in main, went back to all defaults for helmet

--- productionServe.js
+++ productionServe.js
@@ -8,16 +8,8 @@ const usersRouter = require('./routes/users');
 const { cookie } = require('express-validator');
 
 const app = express();
-//app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      scriptSrc: ["'self'", "'sha256-OvqKZ9cjYHWKUBBRfJn1jQUTQQkkM00VwLgFsOnDAXM='", "https://www.google-analytics.com", "https://ssl.google-analytics.com"],
-      "img-src": ["'self'", "data:", "https://www.google-analytics.com"],
-      "connect-src": ["'self'", "https://www.google-analytics.com"]
-    },
-  })
-);
+app.use(helmet());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

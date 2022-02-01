4ca15f5ce1a18bacf028165ce369529189315054
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-02-01 12:41:52 -0600

    added console.log to check value of x-forwarded-proto header before check that same value in if statement

--- productionServe.js
+++ productionServe.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 })
 /*app.set('trust proxy', 'true')*/
 app.use((req, res, next) => {
+  console.log('x-forwarded-proto header value: ' + req.get('x-forwarded-proto'))
   if (req.get('x-forwarded-proto') === "https") {
     next()
   }

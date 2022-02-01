62aa19a31b0ec9525469f75c9d3e1c62b67d7025
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-02-01 14:23:43 -0600

    added === as test

--- productionServe.js
+++ productionServe.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 /*app.set('trust proxy', 'true')*/
 app.use((req, res, next) => {
   console.log('x-forwarded-proto header value: ' + req.get('x-forwarded-proto'))
-  if (req.get('x-forwarded-proto') == "https") {
+  if (req.get('x-forwarded-proto') === "https") {
     console.log('entered if statement')
     return next()
   }
